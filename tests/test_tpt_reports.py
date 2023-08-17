1844d4acaaa4806385a7c758c790ee9bd67f5745
Amelia Vance <amelia.vance@associates.cisa.dhs.gov>
2023-08-17 14:11:51 -0400

    Remove div_params from test_tpt_reports.py

--- tests/test_tpt_reports.py
+++ tests/test_tpt_reports.py
@@ -13,13 +13,6 @@ import pytest
 # cisagov Libraries
 import tpt_reports
 
-div_params = [
-    (1, 1, 1),
-    (2, 2, 1),
-    (0, 1, 0),
-    (8, 2, 4),
-]
-
 log_levels = (
     "debug",
     "info",
