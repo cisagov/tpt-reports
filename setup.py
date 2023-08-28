4dfda3b24c2e4c5b213209784e4e8081e8311c5c
Nick Viola <6486652+nickviola@users.noreply.github.com>
2023-08-28 10:04:21 -0500

    Update project package keywords in setup.py
    Co-authored-by: schmelz21 <81192860+schmelz21@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     ],
     python_requires=">=3.6",
     # What does your project relate to?
-    keywords="skeleton",
+    keywords="Technical Payload Testing (TPT) Report",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={"tpt_reports": ["fonts/*.ttf", "assets/*.png"]},
