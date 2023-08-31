88dbc9ac49d9d417ca1412e53e11d0fc7a43de9c
JCantu248 <JCantu248@gmail.com>
2023-08-31 15:04:55 -0500

    Alphabetizing line 101-104 in tpt_reports.py
    Co-authored-by: dav3r <david.redmin@trio.dhs.gov>

--- src/tpt_reports/tpt_reports.py
+++ src/tpt_reports/tpt_reports.py
@@ -98,10 +98,10 @@ def parse_json(data):
         payloads_meta["host_not_blocked"] = host_not_blocked
         payloads_meta["num_blocked"] = num_payloads
         payloads_meta["num_payloads"] = num_payloads
-        payloads_not_blocked = border_not_blocked + host_not_blocked
-        payloads_meta["payloads_not_blocked"] = payloads_not_blocked
         payloads_blocked = border_blocked + host_blocked
         payloads_meta["payloads_blocked"] = payloads_blocked
+        payloads_not_blocked = border_not_blocked + host_not_blocked
+        payloads_meta["payloads_not_blocked"] = payloads_not_blocked
 
     except Exception as e:
         LOGGER.exception(str(e))
